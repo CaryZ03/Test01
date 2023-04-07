fc9263ad0baebe0a475ba4219064f17a9e6b98b6
zhangkr <Zhangkairui250@126.com>
2023-04-07 23:13:10 +0800

    1

--- 21371469_张锴睿_第4次实验/21371469_张锴睿_第4次实验.sql
+++ 21371469_张锴睿_第4次实验/21371469_张锴睿_第4次实验.sql
@@ -1,20 +1,24 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2023/4/7 21:59:00                            */
+/* Created on:     2023/4/7 22:52:22                            */
 /*==============================================================*/
 
 
 drop table if exists 习题;
 
+drop table if exists 封禁;
+
 drop table if exists 已登录用户;
 
-drop table if exists 游客;
+drop table if exists 用户;
 
 drop table if exists 管理员;
 
-drop table if exists 视频课程;
+drop table if exists 视频;
 
 drop table if exists 认证证书;
+
+drop table if exists 课程;
 
 drop table if exists 错题;
 
@@ -36,6 +40,16 @@ create table 习题
 );
 
 /*==============================================================*/
+/* Table: 封禁                                                    */
+/*==============================================================*/
+create table 封禁
+(
+   管理员id                int not null,
+   用户id                 int not null,
+   primary key (管理员id, 用户id)
+);
+
+/*==============================================================*/
 /* Table: 已登录用户                                                 */
 /*==============================================================*/
 create table 已登录用户
@@ -50,9 +64,9 @@ create table 已登录用户
 );
 
 /*==============================================================*/
-/* Table: 游客                                                    */
+/* Table: 用户                                                    */
 /*==============================================================*/
-create table 游客
+create table 用户
 (
    用户id                 int not null,
    primary key (用户id)
@@ -70,20 +84,21 @@ create table 管理员
 );
 
 /*==============================================================*/
-/* Table: 视频课程                                                  */
+/* Table: 视频                                                    */
 /*==============================================================*/
-create table 视频课程
+create table 视频
 (
-   课程id                 int not null,
+   视频id                 int not null,
    管理员id                int,
+   课程id                 int,
    用户id                 int,
    已登录_用户id             int,
-   课程名                  char(20) not null,
+   视频名                  char(20) not null,
    播放量                  int not null,
    审核人员                 char(20),
    审核日期                 datetime,
    审核结果                 char(10) not null,
-   primary key (课程id)
+   primary key (视频id)
 );
 
 /*==============================================================*/
@@ -96,6 +111,17 @@ create table 认证证书
    用户id                 int,
    完成日期                 date not null,
    primary key (证书编号)
+);
+
+/*==============================================================*/
+/* Table: 课程                                                    */
+/*==============================================================*/
+create table 课程
+(
+   课程id                 int not null,
+   课程名                  char(20) not null,
+   观看人数                 int not null,
+   primary key (课程id)
 );
 
 /*==============================================================*/
@@ -128,31 +154,40 @@ create table 错题集
 );
 
 alter table 习题 add constraint FK_做题 foreign key (用户id)
-      references 游客 (用户id) on delete restrict on update restrict;
+      references 用户 (用户id) on delete restrict on update restrict;
 
 alter table 习题 add constraint FK_用户发布习题 foreign key (已登录_用户id)
       references 已登录用户 (用户id) on delete restrict on update restrict;
 
 alter table 习题 add constraint FK_课程习题 foreign key (课程id)
-      references 视频课程 (课程id) on delete restrict on update restrict;
+      references 课程 (课程id) on delete restrict on update restrict;
 
-alter table 已登录用户 add constraint FK_登录 foreign key (用户id)
-      references 游客 (用户id) on delete restrict on update restrict;
-
-alter table 视频课程 add constraint FK_用户发布视频 foreign key (已登录_用户id)
+alter table 封禁 add constraint FK_管理员 foreign key (用户id)
       references 已登录用户 (用户id) on delete restrict on update restrict;
 
-alter table 视频课程 add constraint "FK_管理员发布/审核视频" foreign key (管理员id)
+alter table 封禁 add constraint FK_被封禁用户 foreign key (管理员id)
       references 管理员 (管理员id) on delete restrict on update restrict;
 
-alter table 视频课程 add constraint FK_观看视频课程 foreign key (用户id)
-      references 游客 (用户id) on delete restrict on update restrict;
+alter table 已登录用户 add constraint FK_登录 foreign key (用户id)
+      references 用户 (用户id) on delete restrict on update restrict;
+
+alter table 视频 add constraint FK_用户发布视频 foreign key (已登录_用户id)
+      references 已登录用户 (用户id) on delete restrict on update restrict;
+
+alter table 视频 add constraint "FK_管理员发布/审核视频" foreign key (管理员id)
+      references 管理员 (管理员id) on delete restrict on update restrict;
+
+alter table 视频 add constraint FK_观看视频课程 foreign key (用户id)
+      references 用户 (用户id) on delete restrict on update restrict;
+
+alter table 视频 add constraint FK_课程视频 foreign key (课程id)
+      references 课程 (课程id) on delete restrict on update restrict;
 
 alter table 认证证书 add constraint FK_获得证书 foreign key (用户id)
       references 已登录用户 (用户id) on delete restrict on update restrict;
 
 alter table 认证证书 add constraint FK_课程证书 foreign key (课程id)
-      references 视频课程 (课程id) on delete restrict on update restrict;
+      references 课程 (课程id) on delete restrict on update restrict;
 
 alter table 错题 add constraint FK_包含 foreign key (错题集id)
       references 错题集 (错题集id) on delete restrict on update restrict;
---
+++ 21371469_张锴睿_第4次实验/21371469_张锴睿_第4次实验_PowerDesigner.md
@@ -0,0 +1,9 @@
+# 设计思路
+
+1. 设计实体![image-20230407225439417](C:\Users\zhangkr\AppData\Roaming\Typora\typora-user-images\image-20230407225439417.png)
+
+   首先将需要单独处理的项都设计成实体。
+
+2. 编辑关系![image-20230407225513893](C:\Users\zhangkr\AppData\Roaming\Typora\typora-user-images\image-20230407225513893.png)
+
+​	按照设计好的实体，编辑实体间的关系，注意一对多和多对多的情况。
