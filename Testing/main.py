84996b0195eddeab9bcbfa460d306696541ecf5f
zhangkr <Zhangkairui250@126.com>
2023-04-27 00:08:00 +0800

    1

--- Testing/main.py
+++ Testing/main.py
@@ -6,17 +6,21 @@ from Black import CheckId
 
 class MyTest(unittest.TestCase):
     # 编写测试用例，记得要以t开头，例：test
-    def testEqual(self):
-        # 调用内部的测试方法，如：assertEqual,assertNotEqual 等
-        self.assertEqual(Triangle.triangle(3, 4, 5), "regular triangle!")
+    # def testEqual(self):
+    #     # 调用内部的测试方法，如：assertEqual,assertNotEqual 等
+    #     self.assertEqual(Triangle.triangle(3, 4, 5), "regular triangle!")
+    #
+    # def testNotEqual(self):
+    #     self.assertEqual(Triangle.triangle("3", 4, 5), "not a triangle!")
 
-    def testNotEqual(self):
-        self.assertEqual(Triangle.triangle("3", 4, 5), "not a triangle!")
-
-    def testId(self):
-        # Black 传入身份证号，调用 exec 返回验证的结果
-        self.assertEqual(CheckId.Black('110221201813010003').exec(), 1)
-        pass
+    def test1(self):
+        self.assertEqual(CheckId.Black('110108202404260002').exec(), 1)
+    def test2(self):
+        self.assertEqual(CheckId.Black('110221201813010005').exec(), 1)
+    def test3(self):
+        self.assertEqual(CheckId.Black('110221201813010006').exec(), 1)
+    def test4(self):
+        self.assertEqual(CheckId.Black('110221201813010007').exec(), 1)
 
 # 运行测试
 if __name__ == '__main__':
