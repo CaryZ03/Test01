857cf62cfbd8c9a735a098b8bf5d61fd34795286
caryz <Zhangkairui250@126.com>
2023-04-28 00:27:57 +0800

    1

--- Testing/main.py
+++ Testing/main.py
@@ -6,21 +6,23 @@ from Black import CheckId
 
 class MyTest(unittest.TestCase):
     # 编写测试用例，记得要以t开头，例：test
-    # def testEqual(self):
-    #     # 调用内部的测试方法，如：assertEqual,assertNotEqual 等
-    #     self.assertEqual(Triangle.triangle(3, 4, 5), "regular triangle!")
-    #
-    # def testNotEqual(self):
-    #     self.assertEqual(Triangle.triangle("3", 4, 5), "not a triangle!")
+    def testNotTriangle1(self):
+        self.assertEqual(Triangle.triangle(3, 3, 3.5), "not a triangle!")
 
-    def test1(self):
-        self.assertEqual(CheckId.Black('110108202404260002').exec(), 1)
-    def test2(self):
-        self.assertEqual(CheckId.Black('110221201813010005').exec(), 1)
-    def test3(self):
-        self.assertEqual(CheckId.Black('110221201813010006').exec(), 1)
-    def test4(self):
-        self.assertEqual(CheckId.Black('110221201813010007').exec(), 1)
+    def testNotTriangle2(self):
+        self.assertEqual(Triangle.triangle(3, 3, 6), "not a triangle!")
+
+    def testEquilateral(self):
+        self.assertEqual(Triangle.triangle(3, 3, 3), "equilateral triangle!")
+
+    def testIsosceles(self):
+        self.assertEqual(Triangle.triangle(3, 3, 4), "isosceles triangle!")
+
+    def testRegular(self):
+        self.assertEqual(Triangle.triangle(3, 4, 5), "regular triangle!")
+
+    # def test1(self):
+    #     self.assertEqual(CheckId.Black('110108999904260007').exec(), 1)
 
 # 运行测试
 if __name__ == '__main__':
