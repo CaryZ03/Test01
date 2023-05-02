5d60b0b24863e8df24b40808fb318dfbbf22bf61
zhangkr <Zhangkairui250@126.com>
2023-05-02 13:04:46 +0800

    1

--- MidTerm/main.py
+++ MidTerm/main.py
@@ -2,138 +2,13 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DDL, text
 import json
+import csv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123@localhost:3306/midterm?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_SORT_KEYS'] = False
 db = SQLAlchemy(app)
-print(db)
-
-
-# 定义模型
-class Department(db.Model):
-    __tablename__ = 'departments'
-    dept_no = db.Column(db.String(4), primary_key=True)
-    dept_name = db.Column(db.String(40), nullable=False, unique=True)
-
-    def to_dict(self):
-        return {
-            'dept_no': self.dept_no,
-            'dept_name': self.dept_name
-        }
-
-
-class Employee(db.Model):
-    __tablename__ = 'employees'
-    emp_no = db.Column(db.Integer, primary_key=True)
-    birth_date = db.Column(db.Date, nullable=False)
-    first_name = db.Column(db.String(14), nullable=False, index=True)
-    last_name = db.Column(db.String(16), nullable=False)
-    gender = db.Column(db.Enum('M', 'F'), nullable=False)
-    hire_date = db.Column(db.Date, nullable=False)
-
-    def to_dict(self):
-        return {
-            'emp_no': self.emp_no,
-            'birth_date': self.birth_date,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'gender': self.gender,
-            'hire_date': self.hire_date
-        }
-
-
-class DeptEmp(db.Model):
-    __tablename__ = 'dept_emp'
-    emp_no = db.Column(db.Integer, db.ForeignKey("employees.emp_no", ondelete='CASCADE'),
-                       primary_key=True)
-    dept_no = db.Column(db.String(4), db.ForeignKey("departments.dept_no", ondelete='CASCADE'),
-                        primary_key=True, index=True)
-    from_date = db.Column(db.Date, nullable=False)
-    to_date = db.Column(db.Date, nullable=False)
-
-    def to_dict(self):
-        return {
-            'emp_no': self.emp_no,
-            'dept_no': self.dept_no,
-            'from_date': self.from_date,
-            'to_date': self.to_date
-        }
-
-
-class DeptManager(db.Model):
-    __tablename__ = 'dept_manager'
-    emp_no = db.Column(db.Integer, db.ForeignKey("employees.emp_no", ondelete='CASCADE'), primary_key=True)
-    dept_no = db.Column(db.String(4), db.ForeignKey("departments.dept_no", ondelete='CASCADE'),
-                        primary_key=True, index=True)
-    from_date = db.Column(db.Date, nullable=False)
-    to_date = db.Column(db.Date, nullable=False)
-
-    def to_dict(self):
-        return {
-            'emp_no': self.emp_no,
-            'dept_no': self.dept_no,
-            'from_date': self.from_date,
-            'to_date': self.to_date
-        }
-
-
-class Title(db.Model):
-    __tablename__ = 'titles'
-    emp_no = db.Column(db.Integer, db.ForeignKey("employees.emp_no", ondelete='CASCADE'), primary_key=True)
-    title = db.Column(db.String(50), primary_key=True)
-    from_date = db.Column(db.Date, primary_key=True)
-    to_date = db.Column(db.Date, nullable=True)
-
-    def to_dict(self):
-        return {
-            'emp_no': self.emp_no,
-            'title': self.title,
-            'from_date': self.from_date,
-            'to_date': self.to_date
-        }
-
-
-class DMT(db.Model):
-    __tablename__ = 'dept_manager_title'
-    emp_no = db.Column(db.Integer, primary_key=True)
-    from_date = db.Column(db.Date, nullable=False)
-    to_date = db.Column(db.Date, nullable=True)
-
-    def to_dict(self):
-        return {
-            'emp_no': self.emp_no,
-            'from_date': self.from_date,
-            'to_date': self.to_date
-        }
-
-
-trigger_create_insert_dmt = DDL(
-    """
-    CREATE TRIGGER trigger_insert_dmt
-    AFTER INSERT ON dept_manager
-    FOR EACH ROW 
-    BEGIN 
-        INSERT INTO dept_manager_title(emp_no, from_date, to_date) VALUES (NEW.emp_no, NEW.from_date, NEW.to_date); 
-    END
-    """
-)
-
-trigger_create_delete_dmt = DDL(
-    """
-    CREATE TRIGGER trigger_delete_dmt
-    AFTER DELETE ON dept_manager
-    FOR EACH ROW 
-    BEGIN 
-        DELETE FROM dept_manager_title WHERE (emp_no, from_date, to_date)=(OLD.emp_no, OLD.from_date, OLD.to_date); 
-    END
-    """
-)
-
-with app.app_context():
-    db.create_all()
-    # db.session.execute(trigger_create_insert_dmt)
-    # db.session.execute(trigger_create_delete_dmt)
 
 
 # 插入数据
@@ -153,13 +28,24 @@ def insert_data(table_name):
 @app.route('/api/v1/<table_name>', methods=['PUT'])
 def update_data(table_name):
     data = request.json
-    p_key = []
-    for key in data:
-        if key.endswith('_no'):
-            p_key.append(key)
     update_values = ', '.join([f"{key} = '{value}'" for key, value in data.items()])
-    where_clause = "WHERE " + " AND ".join([f"{key} = '{value}'" for key, value in data.items() if key in p_key])
-    db.session.execute(text(f'UPDATE {table_name} SET {update_values} {where_clause}'))
+    match table_name:
+        case 'departments':
+            dept_no = data['dept_no']
+            where_clause = f'dept_no = {dept_no}'
+        case 'employees':
+            emp_no = data['emp_no']
+            where_clause = f'emp_no = {emp_no}'
+        case 'dept_emp' | 'dept_manager':
+            emp_no = data['emp_no']
+            dept_no = data['dept_no']
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no}'
+        case 'titles':
+            emp_no = data['emp_no']
+            dept_no = data['dept_no']
+            from_date = data['from_date']
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no} AND from_date = {from_date}'
+    db.session.execute(text(f'UPDATE {table_name} SET {update_values} WHERE {where_clause};'))
     db.session.commit()
     return jsonify({'message': 'hihihi'}), 200
 
@@ -167,44 +53,55 @@ def update_data(table_name):
 # 删除数据
 @app.route('/api/v1/<table_name>/<path:no>', methods=['DELETE'])
 def delete_data(table_name, no):
-    if table_name == 'dept_emp':
-        emp_no, dept_no = no.split('/')
-        print(emp_no, dept_no)
-        db.session.execute(text(f'DELETE FROM {table_name} WHERE emp_no={emp_no} AND dept_no={dept_no}'))
-    elif table_name == 'departments':
-        db.session.execute(text(f'DELETE FROM {table_name} WHERE dept_no={no};'))
-    else:
-        db.session.execute(text(f'DELETE FROM {table_name} WHERE emp_no={no};'))
+    match table_name:
+        case 'departments':
+            where_clause = f'dept_no = {no}'
+        case 'employees':
+            where_clause = f'emp_no = {no}'
+        case 'dept_emp' | 'dept_manager':
+            emp_no, dept_no = no.split('/')
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no}'
+        case 'titles':
+            emp_no, dept_no, from_date = no.split('/')
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no} AND from_date = {from_date}'
+    db.session.execute(text(f'DELETE FROM {table_name} WHERE {where_clause};'))
     db.session.commit()
     return jsonify({'message': 'hihihihi'}), 204
 
 
 # 查询数据
-@app.route('/api/v1/<path:loc>', methods=['GET'])
-def select_data(loc):
-    lenl = len(loc.split('/'))
-    if lenl == 3:
-        table_name, emp_no, dept_no = loc.split('/')
-        print(emp_no, dept_no)
-        query = text(f'SELECT * FROM {table_name} WHERE emp_no={emp_no} AND dept_no={dept_no}')
-    elif lenl == 2:
-        table_name, no = loc.split('/')
-        if table_name == 'departments':
-            query = text(f'SELECT * FROM {table_name} WHERE dept_no={no};')
-        else:
-            query = text(f'SELECT * FROM {table_name} WHERE emp_no={no};')
-    else:
-        # print(request)
-        condition = request.args
+@app.route('/api/v1/<table_name>/<path:no>', methods=['GET'])
+def select_data_with_pri(table_name, no):
+    match table_name:
+        case 'departments':
+            where_clause = f'dept_no = {no}'
+        case 'employees':
+            where_clause = f'emp_no = {no}'
+        case 'dept_emp' | 'dept_manager':
+            emp_no, dept_no = no.split('/')
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no}'
+        case 'titles':
+            emp_no, dept_no, from_date = no.split('/')
+            where_clause = f'emp_no = {emp_no} AND dept_no = {dept_no} AND from_date = {from_date}'
+    result = db.session.execute(text(f'SELECT * FROM {table_name} WHERE {where_clause};'))
+    rows = [dict(zip(result.keys(), row)) for row in result.fetchall()]
+    return jsonify({'rows': rows}), 200
+
+
+# 条件查询数据
+@app.route('/api/v1/<table_name>', methods=['GET'])
+def select_data_with_cond(table_name):
+    condition = request.args
+    if condition:
         key = list(condition.keys())[0]
         value = condition.get(key)
-        table_name = loc
-        query = text(f'SELECT * FROM {table_name} WHERE {key}={value};')
+        query = text(f'SELECT * FROM {table_name} WHERE {key}="{value}";')
+    else:
+        query = text(f'SELECT * FROM {table_name};')
     result = db.session.execute(query)
-    rows = [dict(row) for row in result]
-    return jsonify({'rows': 1}), 200
+    rows = [dict(zip(result.keys(), row)) for row in result.fetchall()]
+    return jsonify({'rows': rows}), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-# f'{len(rows)} rows inserted into {table_name} successfully'
+    app.run(debug=False)
